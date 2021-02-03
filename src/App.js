75df6e57eb57efb8086abbf7132f27726beb4351
wildanGhifari <wildghifari@gmail.com>
2021-02-04 01:21:00 +0700

    re-install package, setting up ChatEngine in App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="46e682f2-0274-4d08-9b49-6657ce360929"
+            userName="wildGhifari"
+            userSecret="wildan123"
+        />
+    )
+}
+
+export default App;
